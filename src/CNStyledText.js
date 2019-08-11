fa06cb8254d78fa546b5835d3fd9c1d53c63467c
Shinpei <32737669+zusinShinpei@users.noreply.github.com>
2019-08-11 21:21:08 +0900

    Update CNStyledText.js

--- src/CNStyledText.js
+++ src/CNStyledText.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {Text, StyleSheet, TextStyle} from 'react-native';
 import _ from 'lodash';
+import ParsedText from "react-native-parsed-text";
+import {COLOR_BLUEBERRY} from "../../../src/constants/color";
+import fonts from "../../../src/constants/fonts";
 
 class CNStyledText extends Component {
   constructor(props) {
@@ -13,7 +16,7 @@ class CNStyledText extends Component {
 
   shouldComponentUpdate(nextProps) {
     if (_.isEqual(this.props.text, nextProps.text)
-            && _.isEqual(this.props.style, nextProps.style)
+        && _.isEqual(this.props.style, nextProps.style)
 
     ) {
       return false;
@@ -25,9 +28,19 @@ class CNStyledText extends Component {
 
   render() {
     return (
-      <Text style={this.props.style}>
-        {this.props.text}
-      </Text>
+        <ParsedText
+            style={this.props.style}
+            parse={[
+              {
+                pattern: /#([0-9a-zA-Z가-힣ㄱ-ㅎ!$%^&*()_+-=<>?]+)/,
+                style: {
+                  color: COLOR_BLUEBERRY,
+                  ...fonts.nanumDefault
+                }
+              }
+            ]}>
+          {this.props.text}
+        </ParsedText>
     );
   }
 }
