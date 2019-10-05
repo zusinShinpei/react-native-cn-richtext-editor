c0e091a570ddafb272043a3cdbaad2a23c3fb737
zusinShinpei <kmh011225@naver.com>
2019-10-05 16:55:44 +0900

    Add

--- src/CNStyledText.js
+++ src/CNStyledText.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
-import {Linking, Text, StyleSheet, TextStyle} from 'react-native';
+import React, { Component } from 'react';
+import { Linking, Text, StyleSheet, TextStyle } from 'react-native';
 import _ from 'lodash';
-import ParsedText from "react-native-parsed-text";
-import {COLOR_BLUEBERRY} from "../../../src/constants/color";
-import fonts from "../../../src/constants/fonts";
+import ParsedText from 'react-native-parsed-text';
+import { COLOR_BLUEBERRY } from '../../../src/constants/color';
+import fonts from '../../../src/constants/fonts';
 
 class CNStyledText extends Component {
   constructor(props) {
@@ -16,7 +16,7 @@ class CNStyledText extends Component {
 
   shouldComponentUpdate(nextProps) {
     if (_.isEqual(this.props.text, nextProps.text)
-        && _.isEqual(this.props.style, nextProps.style)
+      && _.isEqual(this.props.style, nextProps.style)
 
     ) {
       return false;
@@ -28,35 +28,43 @@ class CNStyledText extends Component {
 
   render() {
     return (
-        <ParsedText
-            style={this.props.style}
-            parse={[
-	    {
-					      type: 'url',
-					      style: {
-						      color: 'blue',
-						      textDecorationLine: 'underline',
-					      },
-					    onPress: async (url: string) => {
-						    console.log(url);
-						    if (/^http/.test(url)) {
-							    await Linking.openURL(url);
-						    } else {
-							    await Linking.openURL('http://' + url);
-						    }
-					    },
-				},
-              {
-                pattern: /#([0-9a-zA-Z가-힣ㄱ-ㅎ!$%^&*()_+-=<>?]+)/,
-                style: {
-                  color: COLOR_BLUEBERRY,
-                  ...fonts.nanumDefault
-                }
-              },
-              
-            ]}>
-          {this.props.text}
-        </ParsedText>
+      <ParsedText
+        style={this.props.style}
+        parse={[
+          {
+            type: 'url',
+            style: {
+              color: 'blue',
+              textDecorationLine: 'underline',
+            },
+            onPress: async (url: string) => {
+              console.log(url);
+              if (/^http/.test(url)) {
+                await Linking.openURL(url);
+              } else {
+                await Linking.openURL('http://' + url);
+              }
+            },
+          },
+          {
+            pattern: /#([0-9a-zA-Z가-힣ㄱ-ㅎ!$%^&*()_+-=<>?]+)/,
+            style: {
+              color: COLOR_BLUEBERRY,
+              ...fonts.nanumDefault
+            },
+            onPress: (text: string) => {
+              if(this.props.enablePress) {
+                this.props.SearchStore.searchValue = text;
+                this.props.navigation.push('SearchResult', {
+                  spec: false
+                });
+              }
+            }
+          },
+
+        ]}>
+        {this.props.text}
+      </ParsedText>
     );
   }
 }
